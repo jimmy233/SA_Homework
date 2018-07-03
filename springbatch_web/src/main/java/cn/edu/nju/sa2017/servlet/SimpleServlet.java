0f26b86dc3c068b35f65888ab93d76863373b79e
jimmy233 <m18684110426@163.com>
2018-07-03 20:55:03 +0800

    initial

---
+++ springbatch_web/src/main/java/cn/edu/nju/sa2017/servlet/SimpleServlet.java
@@ -0,0 +1,57 @@
+package cn.edu.nju.sa2017.servlet;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -4751096228274971485L;
+
+	@Override
+	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) throws ServletException, IOException {
+
+		String[] springConfig =
+				{
+						"spring/batch/jobs/job-hello-world.xml"
+				};
+
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext(springConfig);
+
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("helloWorldJob");
+
+		try {
+
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Exit Status : " + execution.getStatus());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+		response.getWriter().println("Done!");
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet " + this.getServletName() + " has started");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("Servlet " + this.getServletName() + " has stopped");
+	}
+	
+}
